0d6bae81500dac01e18aacb2d32a4423723fb723
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-05-06 15:45:25 +0530

    feat: create basic layout of the project

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,57 @@ function App() {
 
 
   return (
-    
-     <>
-      <h1>Hello Sam!!</h1>
-     </>
+
+    <>
+      <div className="main-container">
+        <h1>Memory Card Game</h1>
+        <div className="score-section">
+          <div className="current-score">Score</div>
+          <div className="best-scrore">Best Score</div>
+          <button>Reset Game</button>
+        </div>
+        <div className="card-section">
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+          <div className="card">
+            A
+          </div>
+
+        </div>
+      </div>
+
+    </>
   )
 }
 
