c781f1efce30cd02daa0e03940961e0767fd1f76
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-05-06 16:05:14 +0530

    feat: create Score component and resue it for score and best score

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
-const Header = ({label}) =>{
-  return(
+const Header = ({ label }) => {
+  return (
     <>
-    <h1 className='game-header'>{label}</h1>
+      <h1 className='game-header'>{label}</h1>
+    </>
+  )
+}
+
+const Score = ({ className, label, value }) => {
+
+  return (
+    <>
+      <div className={className}>
+        <p>{label}:</p>
+        <p>{value}</p>
+      </div>
     </>
   )
 }
 
 function App() {
 
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0)
 
   return (
 
     <>
       <div className="main-container">
-        <Header label="Memory Card Game"/>
+        <Header label="Memory Card Game" />
         <div className="score-section">
-          <div className="current-score">Score</div>
-          <div className="best-scrore">Best Score</div>
+          <Score className="current-score" label="Score" value={score} />
+          <Score className="best-score" label="Best Score" value={bestScore} />
           <button>Reset Game</button>
         </div>
         <div className="card-section">
