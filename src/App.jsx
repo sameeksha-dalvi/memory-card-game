ca8d6523c48b44fdb5fcd00a26cb2d02e113f25f
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-05-09 15:29:20 +0530

    style: remove character not availble in ghibli poeple api and replace with some other charactors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import calciferImg from './assets/images/Calcifer.png';
+import pazuImg from './assets/images/Pazu.png';
 import chihiroImg from './assets/images/Chihiro.png';
 import hakuImg from './assets/images/Haku.png';
 import jijiImg from './assets/images/Jiji.png';
 import kikiImg from './assets/images/Kiki.png';
 import meiImg from './assets/images/Mei_Kusakabe.png';
 import muskaImg from './assets/images/Muska.png';
-import nofaceImg from './assets/images/No-Face.png';
+import sanImg from './assets/images/San.png';
 import satsukiImg from './assets/images/Satsuki_Kusakabe.png';
-import sheetaImg from './assets/images/Sheeta.png';
-import sophieImg from './assets/images/Sophie_Elder.png';
+import ursulaImg from './assets/images/Ursula.png';
+import yukiImg from './assets/images/Yuki.png';
 import totoroImg from './assets/images/Totoro.png';
 
 const Header = ({ label }) => {
@@ -68,19 +68,18 @@ function App() {
           <Button label="Reset Game" className="reset-btn" />
         </div>
         <div className="card-section">
-          <Card label="Calcifer" imgName={calciferImg} />
-          <Card label="Chihiro" imgName={chihiroImg} />
+          <Card label="Chihiro Ogino" imgName={chihiroImg} />
           <Card label="Haku" imgName={hakuImg} />
           <Card label="Jiji" imgName={jijiImg} />
           <Card label="Kiki" imgName={kikiImg} />
-          <Card label="Mei" imgName={meiImg} />
-          <Card label="Muska" imgName={muskaImg} />
-          <Card label="No Face" imgName={nofaceImg} />
-          <Card label="Satsuki" imgName={satsukiImg} />
-          <Card label="Sheeta" imgName={sheetaImg} />
-          <Card label="Sophie" imgName={sophieImg} />
+          <Card label="Mei Kusakabe" imgName={meiImg} />
+          <Card label="Colonel Muska" imgName={muskaImg} />
+          <Card label="Satsuki Kusakabe" imgName={satsukiImg} />
           <Card label="Totoro" imgName={totoroImg} />
-
+          <Card label="Pazu" imgName={pazuImg} />
+          <Card label="San" imgName={sanImg} />
+          <Card label="Ursula" imgName={ursulaImg} />
+          <Card label="Yuki" imgName={yukiImg} />
         </div>
       </div>
 
